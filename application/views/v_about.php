58796dee7e03a94c7ca9b09d7807c5b96bbb7c7d
vanisyyyyy <vanisidiyanto12345@gmail.com>
2022-10-04 14:27:29 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Vani</li>
+        <li>Nama Belakang : Sidiyanto</li>
+    </ul> <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Karangtalun</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Purbalingga</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Rebahan</li>
+        <li>Reabhan</li>
+    </ul>
+</section>
